41df1d907fc6b5130c806ed849db956be23a7aae
Jasurbek Khamroev <khamroevapple@gmail.com>
2025-12-01 16:52:04 +0100

    Initial Commit

---
+++ src/main/java/com/LastCoderBoy/telegram_youtube_bot/TelegramYoutubeBotApplication.java
@@ -0,0 +1,13 @@
+package com.LastCoderBoy.telegram_youtube_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramYoutubeBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramYoutubeBotApplication.class, args);
+	}
+
+}
